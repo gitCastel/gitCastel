c2296908fbb73c01e1fd817aac562d2875e88dbe
nicoluchian <nicolaeluchi@gmail.com>
2019-01-31 18:57:27 +0100

    Annadiendo funcionalidad

--- src/bucles/java/EX_01.java
+++ src/bucles/java/EX_01.java
@@ -11,7 +11,9 @@ package bucles.java;
  */
 public class EX_01 {
      public static void main(String[] args) {
-         
+          for(int i=0; i<=1000; i++){
+          		  System.out.println(i);
+        }
         for(int i=0; i<=1000; i++){
             System.out.println(i);
         }
